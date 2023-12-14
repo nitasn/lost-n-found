70b8540656ac15b20bd690038f8f8328f32706e5
nitsan <nitsanbh@gmail.com>
2023-12-15 00:23:22 +0200

    progress (pin animation)

--- src/components/LocationChooser.js
+++ src/components/LocationChooser.js
@@ -12,7 +12,6 @@ import {
   TextInput,
 } from "react-native";
 import { useEffect, useRef, useState } from "react";
-import scopeCircle from "../../assets/scope-circle.png";
 import globalStyles from "../js/globalStyles";
 import { Ionicons } from "@expo/vector-icons";
 import { getLocation } from "../js/location";
@@ -27,25 +26,89 @@ export default function ({ region, setRegion, doClose }) {
   // then we setRegion(pinRegion)
   const [pinRegion, setPinRegion] = useState(region);
 
+  const { current: loop } = useRef(new Animated.Value(0));
+  const { current: once } = useRef(new Animated.Value(0));
+
+  const [isMovingAround, setIsMovingAround] = useState(false);
+  const [isFirstRender, setIsFirstRender] = useState(true);
+
+  const startPinAnimations = () => {
+    Animated.parallel([
+      Animated.spring(once, {
+        toValue: 1,
+        friction: 8,
+        useNativeDriver: true,
+      }),
+      Animated.loop(
+        Animated.sequence([
+          Animated.timing(loop, {
+            toValue: 1,
+            duration: 500,
+            useNativeDriver: true,
+          }),
+          Animated.timing(loop, {
+            toValue: 0,
+            duration: 500,
+            useNativeDriver: true,
+          }),
+        ])
+      ),
+    ]).start();
+  };
+
+  const stopPinAnimations = () => {
+    Animated.parallel([
+      Animated.timing(loop, {
+        toValue: 0,
+        duration: 200,
+        useNativeDriver: true,
+      }),
+      Animated.spring(once, {
+        toValue: 0,
+        friction: 8,
+        useNativeDriver: true,
+      }),
+      ,
+    ]).start();
+  };
+
+  const pinAnimtedStyle = {
+    transform: [
+      {
+        translateY: loop.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, -9],
+        }),
+      },
+      {
+        scale: once.interpolate({
+          inputRange: [0, 1],
+          outputRange: [1, 1.2],
+        }),
+      },
+    ],
+    transformOrigin: "bottom",
+    opacity: once.interpolate({
+      inputRange: [0, 1],
+      outputRange: [1, 0.8],
+    }),
+  };
+
   useEffect(() => {
-    console.log(pinRegion);
-  }, [pinRegion]);
+    if (!isFirstRender) {
+      isMovingAround ? startPinAnimations() : stopPinAnimations();
+    }
+  }, [isMovingAround, isFirstRender]);
 
   return (
     <View style={styles.locationPicker}>
       <SafeAreaView style={styles.header}>
-        <View style={styles.headerInputs}>
+        <View style={styles.headerRow}>
           <TouchableOpacity style={[styles.btn, styles.btn_CANCEL]} onPress={doClose}>
-            <Ionicons size={22} color="red" name="close" />
+            <Ionicons size={26} color="gray" name="chevron-back" />
           </TouchableOpacity>
 
-          <TextInput
-            placeholder="Type Location..."
-            style={styles.input}
-            placeholderTextColor="gray"
-            value={text}
-            onChangeText={setText}
-          />
+          <Text style={styles.headerTitle}>Choose Location</Text>
 
           <TouchableOpacity
             style={[styles.btn, styles.btn_OK]}
@@ -54,9 +117,16 @@ export default function ({ region, setRegion, doClose }) {
               doClose();
             }}
           >
-            <Ionicons size={22} color="green" name="checkmark" />
+            <Ionicons size={26} color="green" name="checkmark" />
           </TouchableOpacity>
         </View>
+        <TextInput
+          placeholder="Type Place Name..."
+          style={styles.input}
+          placeholderTextColor="gray"
+          value={text}
+          onChangeText={setText}
+        />
       </SafeAreaView>
 
       <MapView
@@ -77,19 +147,25 @@ export default function ({ region, setRegion, doClose }) {
           googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
         })}
         region={pinRegion}
-        onRegionChangeComplete={setPinRegion}
+        onRegionChange={() => {
+          isFirstRender ? setIsFirstRender(false) : setIsMovingAround(true);
+        }}
+        onRegionChangeComplete={(region) => {
+          setIsMovingAround(false);
+          setPinRegion(region);
+        }}
         minZoomLevel={0}
         maxZoomLevel={17}
         showsCompass={false}
       />
-      <View style={styles.pinWrapper}>
-        <Image style={styles.pinImg} source={scopeCircle} />
-      </View>
+      <Animated.View style={[styles.pinWrapper, pinAnimtedStyle]}>
+        <Image source={require("../../assets/pin.png")} style={styles.pinImage} />
+      </Animated.View>
     </View>
   );
 }
 
-const pinImgSize = 200;
+const pinImgSize = 48;
 
 const styles = StyleSheet.create({
   locationPicker: {
@@ -103,27 +179,30 @@ const styles = StyleSheet.create({
   header: {
     ...globalStyles.shadow_3,
   },
-  headerInputs: {
+  headerTitle: {
+    fontSize: 22,
+    // fontWeight: 900,
+  },
+  headerRow: {
     padding: 12,
     gap: 12,
     flexDirection: "row",
-    justifyContent: "stretch",
+    justifyContent: "space-between",
+    // alignItems: "center",
   },
   btn: {
     flexDirection: "row",
     alignItems: "center",
-    paddingVertical: 8,
-    paddingHorizontal: 12,
-    ...globalStyles.shadow_3,
-    borderRadius: 5,
+    paddingHorizontal: 4,
   },
   input: {
+    minHeight: 40,
     paddingVertical: 10,
     paddingHorizontal: 12,
     borderWidth: 1,
     borderRadius: 5,
     color: "black",
-    flex: 1,
+    margin: 12,
   },
   mapView: {
     flex: 1,
@@ -132,12 +211,12 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: "50%",
     left: "50%",
-    marginTop: -pinImgSize / 2,
     marginLeft: -pinImgSize / 2,
-    opacity: 0.3,
+    marginTop: -pinImgSize / 2,
+    opacity: 0.9,
     pointerEvents: "none",
   },
-  pinImg: {
+  pinImage: {
     width: pinImgSize,
     height: pinImgSize,
   },
