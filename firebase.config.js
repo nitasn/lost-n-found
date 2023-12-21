b3898899692e60059ac0aa5eb8981f09f0bdd1e3
nitsan <nitsanbh@gmail.com>
2023-12-21 15:20:14 +0200

    google auth works

--- firebase.config.js
+++ firebase.config.js
@@ -1,16 +1,35 @@
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey:             process.env.firebase_apiKey,
-  authDomain:         process.env.firebase_authDomain,
-  projectId:          process.env.firebase_projectId,
-  storageBucket:      process.env.firebase_storageBucket,
-  messagingSenderId:  process.env.firebase_messagingSenderId,
-  appId:              process.env.firebase_appId,
+  apiKey: process.env.firebase_apiKey,
+  authDomain: process.env.firebase_authDomain,
+  projectId: process.env.firebase_projectId,
+  storageBucket: process.env.firebase_storageBucket,
+  messagingSenderId: process.env.firebase_messagingSenderId,
+  appId: process.env.firebase_appId,
 };
 
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// const firebaseConfig = {
+//   apiKey:             process.env.firebase_apiKey,
+//   authDomain:         process.env.firebase_authDomain,
+//   projectId:          process.env.firebase_projectId,
+//   storageBucket:      process.env.firebase_storageBucket,
+//   messagingSenderId:  process.env.firebase_messagingSenderId,
+//   appId:              process.env.firebase_appId,
+// };
+
+// export const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
 
--- src/components/SettingsStack.js
+++ src/components/SettingsStack.js
@@ -1,30 +1,45 @@
-import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import UncontrolledTextInput from "./UncontrolledTextInput";
 
-export default function SettingsStack() {
-  return (
-    <View style={styles.container}>
-      <Text>גבינה צהובה</Text>
-      <UncontrolledTextInput
-        onSubmitEditing={({ nativeEvent: { text } }) => {
-          alert(text);
-        }}
-        placeholder="Type Shomething..."
-        style={styles.input}
-      />
-    </View>
-  );
-}
+import SignInScreen from "./SignInScreen.js";
+import * as React from "react";
+import * as Google from "expo-auth-session/providers/google";
+import * as WebBrowser from "expo-web-browser";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
-  },
-  input: {
-    marginVertical: 24,
-  },
-});
+import { GoogleAuthProvider, onAuthStateChanged, signInWithCredential } from "firebase/auth";
+import { auth } from "../../firebase.config.js";
+
+const { type, message } = WebBrowser.maybeCompleteAuthSession();
+console.log("maybeCompleteAuthSession:", { type, message });
+
+export default function () {
+  const [userInfo, setUserInfo] = React.useState();
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    iosClientId: process.env.firebase_iosClientId,
+    androidClientId: process.env.firebase_androidClientId,
+  });
+
+  React.useEffect(() => {
+    if (response?.type === "success") {
+      const { id_token } = response.params;
+      const credential = GoogleAuthProvider.credential(id_token);
+      signInWithCredential(auth, credential);
+    }
+  }, [response]);
+
+  React.useEffect(() => {
+    const unsub = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        console.log(JSON.stringify(user, null, 2));
+        setUserInfo(user);
+      } else {
+        console.log("user not authenticated");
+      }
+    });
+    return () => unsub;
+  }, []);
+
+  if (userInfo) {
+    return <Text style={{ padding: 30 }}>{JSON.stringify(userInfo, null, 2)}</Text>;
+  } else return <SignInScreen promptAsync={promptAsync} />;
+}
