f6eb2aca82429318c84a9041859b5d4cc8d36754
nitsan <nitsanbh@gmail.com>
2023-12-30 05:30:06 +0200

    using getToken the correct way

--- src/login-social/login.js
+++ src/login-social/login.js
@@ -31,7 +31,7 @@ async function maybeSignUpOnServer(user) {
   if (oldJson === newJson) return;
 
   // we've got a new user!
-  const token = user.stsTokenManager.accessToken;
+  const token = await auth.currentUser.getIdToken();
   const res = await fetch(`${process.env.ServerUrl}/api/sign-up?token=${token}`);
   const json = await res.json();
   if (!res.ok) return console.error("could not sign up!", json);
