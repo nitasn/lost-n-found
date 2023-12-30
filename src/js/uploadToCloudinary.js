4c5f78a4ece7fcfdaa4d45fc07b2a76db49b6361
nitsan <nitsanbh@gmail.com>
2023-12-30 07:19:50 +0200

    actually upload

--- src/js/uploadToCloudinary.js
+++ src/js/uploadToCloudinary.js
@@ -7,17 +7,12 @@ const uploadUrl = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
 
 // todo can i do signed upload (with credentials??)
 
-function randomIntBetween(min, max) {
-  // min and max included
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
 /**
  * @returns {Promise<string?>} url of the hosted image, or null if err.
  */
 export default async function uploadToCloudinary(filedata) {
-  await sleep(500);
-  return "test--not-url"; // TODO DELETE THIS!
+  // await sleep(500);
+  // return "test--not-url"; // TODO DELETE THIS!
 
   const formData = new FormData();
   formData.append("file", filedata);
