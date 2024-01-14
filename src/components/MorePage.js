bfaf9604000cc80e25c5f0db3e1c90e7673061f5
nitsan <nitsanbh@gmail.com>
2024-01-14 13:02:09 +0200

    added plz login msg

--- src/components/MorePage.js
+++ src/components/MorePage.js
@@ -1,47 +1,57 @@
 import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import LogInOutGoogle from "../login-social/LogInOutGoogle.js";
 import { auth } from "../../firebase.config.js";
-import { Ionicons } from "@expo/vector-icons";
-import { primaryColor } from "../js/theme.js";
-import globalStyles from "../js/globalStyles.js";
 import Hr from "./Hr.js";
 import alerto from "./Alerto.js";
 import { useNavigation } from "@react-navigation/native";
 
 import MenuItem from "./MenuItem.js";
+import { useAuth } from "../login-social/login.js";
 
 export default function MorePage() {
   const navigation = useNavigation();
+  const [user] = useAuth();
 
   const onPressing = (type) => () => {
-    if (!auth.currentUser) {
-      return alerto({
-        title: "To Upload, Please Sign In",
-        message: "To upload posts, and to enable chat with other users, please sign in ❤️",
-      });
-    }
     navigation.navigate("PostComposer", { type });
   };
+
   return (
     <View style={styles.screen}>
-      <View style={styles.menuItemsContainer}>
-        <MenuItem iconName="earth-outline" text="Upload Found Item" onPress={onPressing("found")} />
-        <MenuItem iconName="planet-outline" text="Upload Lost Item" onPress={onPressing("lost")} />
-      </View>
-      <Hr style={styles.hr} />
+      {!user && <MsgLogInFirst />}
       <LogInOutGoogle />
+      <Hr style={styles.hr} />
+      <View style={styles.menuItemsContainer}>
+        <MenuItem
+          iconName="earth-outline"
+          text="Upload Found Item"
+          onPress={onPressing("found")}
+          disabled={!user}
+        />
+        <MenuItem
+          iconName="planet-outline"
+          text="Upload Lost Item"
+          onPress={onPressing("lost")}
+          disabled={!user}
+        />
+      </View>
     </View>
   );
 }
+
+const MsgLogInFirst = () => {
+  return <Text style={styles.msgLogInFirst}>To Upload and to Chat, Please Sign In ❤️</Text>;
+};
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 24,
   },
-  h2: {
-    fontSize: 24,
-    marginVertical: 12,
+  msgLogInFirst: {
+    marginBottom: 16,
+    color: "hsl(0, 0%, 15%)",
+    textAlign: "center",
   },
   hr: {
     marginVertical: 24,
