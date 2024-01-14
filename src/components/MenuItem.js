ebae02839b40c13b91b759e51592740697c54c67
nitsan <nitsanbh@gmail.com>
2024-01-14 12:45:16 +0200

    added disabled option

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -6,12 +6,24 @@ import globalStyles from "../js/globalStyles.js";
 /**
  * @param {{ iconName: keyof typeof Ionicons.glyphMap }}
  */
-export default function MenuItem({ iconName, text, onPress }) {
+export default function MenuItem({ iconName, text, onPress, disabled }) {
   return (
-    <TouchableOpacity style={styles.menuItem} onPress={onPress}>
-      <Ionicons name={iconName} size={24} color={primaryColor} />
-      <Text style={styles.menuItemText}>{text}</Text>
-      <Ionicons name="chevron-forward" size={24} color={primaryColor} />
+    <TouchableOpacity
+      style={[styles.menuItem, disabled && styles.menuItemDisabled]}
+      onPress={onPress}
+      disabled={disabled}
+    >
+      <Ionicons
+        name={iconName}
+        size={24}
+        color={!disabled ? styles.icon.color : styles.iconDisabled.color}
+      />
+      <Text style={[styles.menuItemText, disabled && styles.menuItemTextDisabled]}>{text}</Text>
+      <Ionicons
+        name="chevron-forward"
+        size={24}
+        color={!disabled ? styles.icon.color : styles.iconDisabled.color}
+      />
     </TouchableOpacity>
   );
 }
@@ -25,7 +37,19 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     ...globalStyles.shadow_1,
   },
+  menuItemDisabled: {
+    backgroundColor: "rgb(229, 229, 229)",
+  },
+  menuItemTextDisabled: {
+    color: "rgb(171, 171, 171)",
+  },
   menuItemText: {
     marginRight: "auto",
+  },
+  icon: {
+    color: primaryColor,
+  },
+  iconDisabled: {
+    color: "rgb(171, 171, 171)",
   },
 });
