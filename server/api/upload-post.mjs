4831c3d7d8e053b81ac93e7c096f1415bc537768
Nitsan BenHanoch <nitsanbh@gmail.com>
2024-09-17 19:29:44 +0300

    tags uploading

--- server/api/upload-post.mjs
+++ server/api/upload-post.mjs
@@ -16,19 +16,22 @@ export default async function (req, res) {
 
   const locationPromise = reverseGecode(latLong);
 
-  const tagsPromise = getImagesTagsFromAI()
+  const tagsPromise = getImagesTagsFromAI(picsUrls)
+    .then((str) => str.split(",").map((desc) => desc.trim()))
+    .catch(() => nulljj);
 
-  const record = { author: decodedToken.uid, title, text, location, picsUrls, type };
+  const [location, tags] = await Promise.all([locationPromise, tagsPromise]);
+
+  const record = { type, author: decodedToken.uid, title, text, location, picsUrls, tags };
 
   try {
     const post = await new Post(record).save();
     return res.status(200).send(post);
-  } 
-  catch (err) {
+  } catch (err) {
     console.error(`api/upload-post: error 400:`, err.message);
     return res.status(400).send({ error: err.message });
   }
-};
+}
 
 async function reverseGecode(latLong) {
   if (!latLong) return null;
@@ -39,11 +42,9 @@ async function reverseGecode(latLong) {
       `latitude=${lat}&longitude=${long}&localityLanguage=en`;
     const json = await fetch(url).then((res) => res.json());
     const name = json.locality || json.city || json.countryName;
-    return { latLong, name }
-  } 
-  catch (err) {
+    return { latLong, name };
+  } catch (err) {
     console.error(`Couldn't Reverse-Gecode [${lat}, ${long}]:`, err.message);
     return null;
   }
 }
-
